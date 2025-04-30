401ea6e3f0a1ec4d479b475dd0439362f0dbc6da
singhcse01 <singhcse01@gmail.com>
2025-04-30 14:35:56 +0530

    added demo scheduler

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -1,5 +1,7 @@
 package com.javatechie.k8s;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringbootK8sDemoApplication {
 
-	@GetMapping("/message")
+	private final Log logger = LogFactory.getLog(getClass());
+
+	@GetMapping("/scheduler")
 	public String displayMessage(){
-		return "Congratulation you successfully deployed your application to kubernetes !!";
-	}
+		logger.info("Scheduler starting at :::::::::::" + System.currentTimeMillis());
+		return "This api is working as a demo scheduler";
+
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootK8sDemoApplication.class, args);
